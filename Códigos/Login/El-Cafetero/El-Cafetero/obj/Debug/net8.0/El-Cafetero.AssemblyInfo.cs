4bfcc91138c1950920d3495398b4a7e3c975fd74
Manuel <gonzalezjuanmanuel645@gmail.com>
2024-10-08 12:08:22 -0500

    Añadiendo informacion y estilo a la pagina

--- Códigos/Login/El-Cafetero/El-Cafetero/obj/Debug/net8.0/El-Cafetero.AssemblyInfo.cs
+++ Códigos/Login/El-Cafetero/El-Cafetero/obj/Debug/net8.0/El-Cafetero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("El-Cafetero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9052e8ece9148c7dda0b118c4c0ac6cab5809226")]
 [assembly: System.Reflection.AssemblyProductAttribute("El-Cafetero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("El-Cafetero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
