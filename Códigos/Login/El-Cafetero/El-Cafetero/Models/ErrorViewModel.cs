99d19da5de970c1ce405ef01acf08aaf0b7d899e
Manuel <gonzalezjuanmanuel645@gmail.com>
2024-10-15 19:49:48 -0500

    Subiendo el login final, conectado con la base de datos modificada de postgres.

--- Códigos/Login/El-Cafetero/El-Cafetero/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace El_Cafetero.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Códigos/Login/El-Cafetero/El-Cafetero/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace El_Cafetero.Models
-{
-    public class Users : IdentityUser
-    {
-        public string FullName { get; set; }
-    }
-}
