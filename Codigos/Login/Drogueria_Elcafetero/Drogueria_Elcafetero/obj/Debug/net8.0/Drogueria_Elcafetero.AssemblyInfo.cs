81f69212056c1300544cacd1fb645653f75bd389
Manuel <gonzalezjuanmanuel645@gmail.com>
2024-11-19 23:12:11 -0500

    .

--- Codigos/Login/Drogueria_Elcafetero/Drogueria_Elcafetero/obj/Debug/net8.0/Drogueria_Elcafetero.AssemblyInfo.cs
+++ Codigos/Login/Drogueria_Elcafetero/Drogueria_Elcafetero/obj/Debug/net8.0/Drogueria_Elcafetero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Drogueria_Elcafetero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd4f6f9e8fcb12957f5b46b50d570d72b1ecd4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed1f327efc89fa220a5e2033d8395423438d8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Drogueria_Elcafetero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Drogueria_Elcafetero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
